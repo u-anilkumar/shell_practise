00268028b8d0a5d613eb247efef029678081ea25
anilkummar <uanilchoudary98@gmail.com>
2026-03-09 21:55:18 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -2,9 +2,9 @@
 #!/bin/bash 
 # above line is called shebang and it is the interpreter of shell script
 
-if [ $1 >> $2 ]; then
+if [ $1 gt $2 ]; then
 echo "$1 is greater than $2"
-elif [ $1==$2 ]; then
+elif [ $1 eq $2 ]; then
 echo "$1 is equal to $2"
 else 
 echo "$1 is less than $2"

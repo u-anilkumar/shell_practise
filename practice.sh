da892bcdd011968ab7ca248d02dc62f3ec2d2bab
anilkummar <uanilchoudary98@gmail.com>
2026-03-09 21:46:50 +0530

    c

--- practice.sh
+++ practice.sh
@@ -4,3 +4,10 @@ echo "Hello Anil"
 SUM=$(($1+$2))
 
 echo "sum is $SUM"
+
+echo "Total num of args passed: $#"
+echo " All vars passed in script: $@"
+echo "current working directoty is $PWD"
+echo "Current user is $USER and user home directory is $HOME"
+echo "PID of script $$"
+echo "name of script $0"

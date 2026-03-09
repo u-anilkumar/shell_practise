fbce4ac2de01430f15ca8589ca45ddf60062acb6
anilkummar <uanilchoudary98@gmail.com>
2026-03-09 22:26:30 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -6,14 +6,15 @@ if [ $1 -gt $2 ]; then
 echo "$1 is greater than $2"
 elif [ $1 -eq $2 ]; then
 echo "$1 is equal to $2"
+echo " multiplication of $1 and $2 is $(($1*$2))"
 else 
 echo "$1 is less than $2"
 fi 
 
-echo " status code of grt less tha $?"
+echo "status code of grt less than is: $?"
 
 if [ $(($1%2)) == 0 ]; then
 echo " $1 is an even number"
-else 
+else
 echo "$1 is an odd number"
 fi

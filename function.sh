23b6778b55e5272d0e5e4d1ce14ae180c25ccd4d
anilkummar <uanilchoudary98@gmail.com>
2026-03-10 17:55:08 +0530

    fn

--- function.sh
+++ function.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 R="\e[31m"
 N="\e[0m"
-G="\e[30m"
+G="\e[32m"
 USER_ID=$(id -u)
  
 echo "user id is: $USER_ID"
 validate()
 {
     if [ $1 -eq 0 ]; then
-    echo -e "$2 $G SUCCESS $N"
+    echo -e "$2 $G SUCCESS"
 
     else
-    echo -e "$2 $RFAILURE $N"
+    echo -e "$2 $R FAILURE"
 
     fi
 }

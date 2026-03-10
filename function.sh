04241f3e8132bd79cbd0424e84b30fa8be3d6069
anilkummar <uanilchoudary98@gmail.com>
2026-03-10 17:43:04 +0530

    fn

--- function.sh
+++ function.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+R="\e[31m"
 USER_ID=$(id -u)
  
 echo "user id is: $USER_ID"
 validate()
 {
     if [ $1 -eq 0 ]; then
-    echo "$2 SUCCESS"
+    echo "$2 $R SUCCESS"
 
     else
     echo "$2 FAILURE"
@@ -17,7 +17,7 @@ validate()
 if [ $USER_ID -ne 0 ]; then
 echo "USER should have sudo permission to run this script" 
 else
-dnf install nginx -y
+dnf install nginx -y 
 validate $? "nginx installation"
 
 fi

e50c38b5185436de8a2b6de632cd90069325af03
anilkummar <uanilchoudary98@gmail.com>
2026-03-10 17:22:22 +0530

    function

---
+++ function.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USER_ID= id -u
+
+echo "user id is: $USER_ID"
+validate(){
+    if [$1 ne 0 ]; then
+    echo "$2 FAILURE"
+
+    else
+    echo "$2 SUCCESS"
+}
+
+dnf install nginix -y
+validate $? nginix installation

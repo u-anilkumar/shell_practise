5214d4543ac9e80616cf2ea82ce631060bf7c32a
anilkummar <uanilchoudary98@gmail.com>
2026-03-10 18:39:33 +0530

    fn

--- logs.sh
+++ logs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mkdir -p /var/log/shell_script
-logFile=$0
-echo "script name is : $logFile" | tee -a logFile
+Log_Folder="/var/log/shell_script"
+mkdir -p $Log_Folder
+logFile="/var/log/shell_script/$0.log"
+echo "script name is : $logFile" | tee -a $logFile
 # below codes are to add colours to improve readability of the script
 R="\e[31m"
 N="\e[0m"
@@ -9,17 +10,17 @@ G="\e[32m"
 #to check userid
 USER_ID=$(id -u) 
  
-echo "user id is: $USER_ID" | tee -a logFile
+echo "user id is: $USER_ID" | tee -a $logFile
 #fnction to validate the status of the command and use for error handlig
 
 validate() 
 {
     # we are passing arguments as $1 and $2
     if [ $1 -eq 0 ]; then
-    echo -e "$2 $G SUCCESS" | tee -a logFile
+    echo -e "$2 $G SUCCESS" | tee -a $logFile
 
     else
-    echo -e "$2 $R FAILURE $N" | tee -a logFile
+    echo -e "$2 $R FAILURE $N" | tee -a $logFile
 
     fi
 }
@@ -31,11 +32,11 @@ else
 for package in $@
 # $@ --> all the arguments passed to the script
 do
-dnf list installed $package &>> logFile
+dnf list installed $package &>> $logFile
 if [ $? -eq 0 ]; then
-echo "$package is already installed" | tee -a logFile
+echo "$package is already installed" | tee -a $logFile
 else
-dnf install $package -y & >> logFile
+dnf install $package -y & >> $logFile
 validate $? "$package installation" 
 fi
 done

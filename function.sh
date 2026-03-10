1b37920dce1df44a77e9a68f8b4470f469503398
anilkummar <uanilchoudary98@gmail.com>
2026-03-10 18:12:43 +0530

    fn

--- function.sh
+++ function.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+
+# below codes are to add colours to improve readability of the script
 R="\e[31m"
 N="\e[0m"
 G="\e[32m"
+#to check userid
 USER_ID=$(id -u)
  
 echo "user id is: $USER_ID"
-validate()
+#fnction to validate the status of the command and use for error handlig
+
+validate() 
 {
+    # we are passing arguments as $1 and $2
     if [ $1 -eq 0 ]; then
     echo -e "$2 $G SUCCESS"
 
@@ -16,10 +22,12 @@ validate()
     fi
 }
 
+#check if user is sudo user 
 if [ $USER_ID -ne 0 ]; then
 echo "USER should have sudo permission to run this script" 
 else
 for package in $@
+# $@ --> all the arguments passed to the script
 do
 dnf list installed $package
 if [ $? -eq 0 ]; then

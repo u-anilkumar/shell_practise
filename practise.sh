cdcdc8a36293fd5a9532b1b4e7c698de0071d8c8
anilkummar <uanilchoudary98@gmail.com>
2026-09-07 23:12:07 +0530

    variables

--- practise.sh
+++ practise.sh
@@ -4,3 +4,13 @@ NUM1=1
 NUM2=3
 SUM=$(($NUM1+$NUM2))
 echo "Total is $SUM"
+
+echo "Current user is $USER"
+echo "$USER Home directory is $HOME"
+echo "Current working directoty is $PWD"
+echo "Script Name is $0"
+echo "PID is $$"
+echo "$(id -u) is the user id."
+echo "Args passed to script are $@"
+echo " Num of args is @#"
+echo "output of previos command is $?"

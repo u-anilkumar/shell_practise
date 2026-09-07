85980d554829c9ccee0d97b07a6c9abb29b0e642
anilkummar <uanilchoudary98@gmail.com>
2026-09-07 23:34:56 +0530

    condition

---
+++ redirection.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+USERID=$(id -u)
+#store logs in /var/log/shell_p
+LOG_DIR=/var/log/shell_p
+LOG_FILE=$LOG_DIR/$0.log
+mkdir -p $LOG_DIR
+
+if [ $USERID -ne 0 ]; then
+    echo "Admin access is needed to run this script" | tee -a $LOG_FILE
+    exit 1
+fi
+
+dnf install nginx -y &>> $LOG_FILE
+
+if [ $? -ne 0 ]; then
+    echo "nginx installation is ... Failure" | tee -a $LOG_FILE
+else 
+    echo "Nginx Installation is .. SUCCESS" | tee -a $LOG_FILE
+fi

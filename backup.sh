b7f905af413917a340ce6cb2efc087bc6e3c7ac8
anilkummar <uanilchoudary98@gmail.com>
2026-09-14 15:14:41 +0530

    backup

---
+++ backup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+source ./common.sh
+LOG_DIR=/var/log/backup/
+LOG_FILE=$LOG_DIR/backup.log
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+#enable colours
+R='\e[31m'
+G='\e[32m'
+N='\e[0m'
+Y='\e[33m'
+
+echo "Current PID is: $$"
+
+
+USAGE()
+{
+    echo -e " $R sudo sh backup.sh <SOURCE DIR> <DEST DIR> <14(days)> $N" | tee -a $LOG_FILE
+    exit 1
+}
+
+CHECK_USER 
+mkdir -p $LOG_DIR 
+
+if [ ! -d "$SOURCE_DIR" ]; then
+    USAGE
+elif [ ! -d "$DEST_DIR" ]; then
+    USAGE
+fi
+
+FILES_FOUND=$(find $SOURCE_DIR -f "*.log" mtime +$DAYS)
+
+if [ -z "$FILES_FOUND" ]; then
+    echo -e "No files found to ARCHIVE...$Y SKIPPING $N"
+else   
+    tar -czvf "$DEST_DIR/applogs_$(date +%Y_%m_%d_%H_%M_%S)" "$FILES_FOUND"
+    if [ $? -ne 0 ];
+        echo -e "$R Archval failed $N"
+    else
+        echo -e "$G ARCHIVAL SUCCESS $N"
+        while IFS=read -r FILES
+        do 
+            echo "deleting $FILES"
+            rm -f "$FILES" 
+            echo "deleted $FILES"
+        done <<<$FILES_FOUND
+    fi
+fi
---
+++ common.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+USER=$(id -u)
+
+#enable colours
+R='\e[31m'
+G='\e[32m'
+N='\e[0m'
+Y='\e[33m'
+
+#check user
+CHECK_USER()
+{
+    if [ USER -ne 0 ]; then
+        echo -e "You do not have permission to run this script"
+        exit 1
+    fi
+}
+
+echo "Current PID is: $$"
